013221bb7aa8a28edc407b9563b2d1e64c5a1420
Fares84 <fares.sassi2015@gmail.com>
2021-04-15 15:19:30 +0000

    fixed file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,32 +18,31 @@ def do_deploy(archive_path):
         archive_path (path): path of the archive file 
     """
     if not path.exists(archive_path):
-          return False
-    # var to get the file name only separated by / and get last pos
-    file_name = archive_path.split("/")[-1].split(".")[0]
-    # set var for path to unompr and set only the compressed file
-    # without the exetension, divided by "." and first[0] position
-    folder_to_compress = "/data/web_static/releases/{}/web_static/*".format(file_name)
+        return False
     try:
-        # uploading the archive_path to tmp on the server
+        # var to get the file name only separated by / and get last pos
+        filename = archive_path.split("/")[-1]
+        # set var for path to unompr and set only the compressed file
+        # without the exetension, divided by "." and first[0] position
+        folderpath = ("/data/web_static/releases/" + 
+                      filename.split(".")[0])
+        # upload the archive_path tp tmp on the server
         put(archive_path, "/tmp/")
         # run is used to execute a shell command on remote servers
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(file_name))
+        run("mkdir -p {}".format(folderpath))
         # uncompress archive to specific folder using -C
         # tar compresses multiple files
-        run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".format(file_name, file_name))
+        run("tar -xzf /tmp/{} -C {}".format(filename,
+                                            folderpath)) 
         # delete the archive from the web server
-        run("sudo rm /tmp/{}.tgz".format(file_name))
-        run("sudo mv {} /data/web_static/releases/{}/".format(folder_to_compress, file_name))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}/".format(folderpath,
+                                            folderpath))
+        run("rm -rf {}/web_static".format(folderpath))
         # delete symbolic link
-        run("sudo rm -rf /data/web_static/releases/{}/web_static".
-            format(file_name))
-        # create a new symbolic link ln -s /usr/local/var /var/run
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(file_name))
-        # on the server, create a linked with the current
+        run("rm -rf /data/web_static/current")
+        #create a new symbolic link ln -s /usr/local/var /var/run
+        run("sudo ln -s {} /data/web_static/current".format(folderpath))
         return True
     except:
         return False
-    return True
