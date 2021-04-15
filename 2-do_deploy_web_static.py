c4dd0f563e8e9f6110220c087f973e16bbc874a8
Fares84 <fares.sassi2015@gmail.com>
2021-04-15 15:34:07 +0000

    fixed file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,53 @@
 #!/usr/bin/python3
-""" Fabric script (based on the file 1-pack_web_static
-that distributes an archive to web server, using
-the function do_deploy"""
-from fabric.api import local
+"""[Fabric script]
+"""
 from fabric.api import env
-from fabric.api import run
 from fabric.api import put
+from fabric.api import run
+from fabric.api import local
 from datetime import datetime
 from os import path
 
-env.hosts = ['34.73.62.68', '34.75.147.36']
+env.hosts = ['34.75.254.10', '34.75.125.125']
 env.user = "ubuntu"
 
-def do_deploy(archive_path):
-    """ distributes an archive to web server
-    Args:
-        archive_path (path): path of the archive file 
+
+def do_pack():
+    """[generates a .tgz archive]
     """
-    if not path.exists(archive_path):
+    time_format = '%Y%m%d%H%M%S'
+    time = datetime.utcnow().strftime(time_format)
+    filepath = "versions/web_static_{}.tgz".format(time)
+    local("mkdir -p versions")
+    local("tar -cvzf {} web_static".format(filepath))
+    if path.exists(filepath):
+        return filepath
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """[distributes an archive to your web servers]
+    Args:
+        archive_path ([path]): [path of the archive file]
+    """
+    if not path.exists(archive_path) and path.isfile(archive_path):
         return False
+    filename = archive_path.split("/")[-1].split(".")[0]
+    path1 = "/data/web_static/releases/{}/web_static/*".format(filename)
     try:
-        # var to get the file name only separated by / and get last pos
-        filename = archive_path.split("/")[-1]
-        # set var for path to unompr and set only the compressed file
-        # without the exetension, divided by "." and first[0] position
-        folderpath = ("/data/web_static/releases/" + 
-                      filename.split(".")[0])
-        # upload the archive_path tp tmp on the server
         put(archive_path, "/tmp/")
-        # run is used to execute a shell command on remote servers
-        run("mkdir -p {}".format(folderpath))
-        # uncompress archive to specific folder using -C
-        # tar compresses multiple files
-        run("tar -xzf /tmp/{} -C {}".format(filename,
-                                            folderpath)) 
-        # delete the archive from the web server
-        run("rm /tmp/{}".format(filename))
-        run("mv {}/web_static/* {}/".format(folderpath,
-                                            folderpath))
-        run("rm -rf {}/web_static".format(folderpath))
-        # delete symbolic link
-        run("rm -rf /data/web_static/current")
-        #create a new symbolic link ln -s /usr/local/var /var/run
-        run("sudo ln -s {} /data/web_static/current".format(folderpath))
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
+            format(filename, filename))
+        run("sudo rm /tmp/{}.tgz".format(filename))
+        run("sudo mv {} /data/web_static/releases/{}/".format(path1, filename))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static".
+            format(filename))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
         return True
     except:
         return False
+    return True
